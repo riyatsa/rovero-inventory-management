8a52a40019c921397cbe98f9071622a9a99b9c7a
Yash Panchal <56638347+riyatsa@users.noreply.github.com>
2023-03-04 17:40:39 +0530

    Update Index

--- index.php
+++ index.php
@@ -9,10 +9,10 @@ $url.= $_SERVER['HTTP_HOST'];
 
 // Append the requested resource location to the URL   
 $url.= $_SERVER['REQUEST_URI'];    
-
 echo '<a target="_blank" href="'.$url.'admin/">Visit Admin</a>';
 echo '<br><a target="_blank" href="'.$url.'franchise/">Visit Franchise</a>';
 echo '<br><a target="_blank" href="'.$url.'store-user/">Visit Store-user</a>'; 
+echo '<br><a target="_blank" href="'.$url.'warehouse/">Visit warehouse</a>'; 
 
 
 ?>
